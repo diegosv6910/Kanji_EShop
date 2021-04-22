15091ef5e94af2f2b8a98ce12f07b26048b31e45
Diego Sandoval <diegosv6910@gmail.com>
2021-04-22 14:42:41 -0500

    Adding query for select productos

--- public/js/backendIndex.js
+++ public/js/backendIndex.js
@@ -1,4 +1,25 @@
+var sqlDetails = require('./mysql')
+var mysql = require('mysql')
+
 function onloadBack() {
-    var itemTitle = document.getElementById("titleItem")
-    itemTitle.innerHTML = "Hola"
+
+    var con = mysql.createConnection(sqlDetails)
+    con.query("SELECT * FROM `"+ process.env.BDNAME +"`.`productos`", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+
+    var arrayItems = ["nombre", "descripcion", "precio", "caracteristica1", "caracteristica2", "caracteristica3", "categoria"];
+
+    var productItems = [
+
+    ]
+
+    console.log(productItems)
+
+    productItems.push(arrayItems)
+    productItems.push(arrayItems)
+
+    console.log(productItems)
+
 }
