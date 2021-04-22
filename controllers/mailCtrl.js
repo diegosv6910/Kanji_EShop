3fc65bb24473d011bb95f5ad47e902edef9a0960
Diego Sandoval <diegosv6910@gmail.com>
2021-04-22 08:40:12 -0500

    Fix Bugs

--- controllers/mailCtrl.js
+++ controllers/mailCtrl.js
@@ -1,6 +1,7 @@
 var nodemailer = require('nodemailer'); // email sender function
 var handlebars = require('handlebars');
 var fs = require('fs-extra');
+const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
