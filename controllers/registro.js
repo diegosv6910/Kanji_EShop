8883ec2568282dde0f78fcd541961bfbd4880ef0
Diego Sandoval <diegosv6910@gmail.com>
2021-04-22 09:27:33 -0500

    Encrypting Passwords

--- controllers/registro.js
+++ controllers/registro.js
@@ -3,12 +3,14 @@
 const sendEmail = require('./mailCtrl')
 var sqlDetails = require('./mysql')
 var mysql = require('mysql')
+var CryptoJS = require("crypto-js");
 
 function guardarUsuario(req, res) {
     var con = mysql.createConnection(sqlDetails);
             let dataForm = [req.body.Name, req.body.last_Name, req.body.Mail, req.body.Phone, req.body.password2]
+            var pwdEncrypt = CryptoJS.AES.encrypt(dataForm[4], process.env.SECRET_KEY).toString();
             res.redirect('/v1')
-            var QueryRegister = "INSERT INTO `"+ process.env.BDNAME +"`.`usuarios` (`nameUser`, `lastnameUser`, `mailUser`, `phoneUser`, `password`) VALUES ('" + dataForm[0] + "', '" + dataForm[1] + "', '" + dataForm[2] + "', '" + dataForm[3] + "', MD5('" +dataForm[4] + "')); "
+            var QueryRegister = "INSERT INTO `"+ process.env.BDNAME +"`.`usuarios` (`nameUser`, `lastnameUser`, `mailUser`, `phoneUser`, `password`) VALUES ('" + dataForm[0] + "', '" + dataForm[1] + "', '" + dataForm[2] + "', '" + dataForm[3] + "', '" + pwdEncrypt + "'); "
             con = mysql.createConnection(sqlDetails);
             con.query(QueryRegister, function (error) {
                 if (error) throw error
