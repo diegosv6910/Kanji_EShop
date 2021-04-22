85f6e07a5419bc6f9e1df306949c57c4c880e74d
Diego Sandoval <diegosv6910@gmail.com>
2021-04-21 19:58:45 -0500

    Fix Bugs MAIL

--- controllers/registro.js
+++ controllers/registro.js
@@ -8,13 +8,14 @@ function guardarUsuario(req, res) {
     var con = mysql.createConnection(sqlDetails);
             let dataForm = [req.body.Name, req.body.last_Name, req.body.Mail, req.body.Phone, req.body.password2]
             sendEmail.sendEmail(req, res, dataForm[2])
-            var QueryRegister = "INSERT INTO `"+ process.env.BDNAME +"`.`usuarios` (`nameUser`, `lastnameUser`, `mailUser`, `phoneUser`, `password`) VALUES ('" + dataForm[0] + "', '" + dataForm[1] + "', '" + dataForm[2] + "', '" + dataForm[3] + "', '" + dataForm[4] + "'); "
-            con = mysql.createConnection(sqlDetails);
-            con.query(QueryRegister, function (error) {
-                if (error) throw error
-            })
+            res.send("Hola mundo");
+            // var QueryRegister = "INSERT INTO `"+ process.env.BDNAME +"`.`usuarios` (`nameUser`, `lastnameUser`, `mailUser`, `phoneUser`, `password`) VALUES ('" + dataForm[0] + "', '" + dataForm[1] + "', '" + dataForm[2] + "', '" + dataForm[3] + "', '" + dataForm[4] + "'); "
+            // con = mysql.createConnection(sqlDetails);
+            // con.query(QueryRegister, function (error) {
+            //     if (error) throw error
+            // })
             
-            con.end();
+            // con.end();
 }
 
 function checkMailRepeat(mail) {
