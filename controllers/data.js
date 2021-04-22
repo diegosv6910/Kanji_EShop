08ce02afc65a5c9b7b7b910b8280c62fa76527ce
Diego Sandoval <diegosv6910@gmail.com>
2021-04-22 15:26:56 -0500

    Recieving Data

--- controllers/data.js
+++ controllers/data.js
@@ -5,9 +5,10 @@ var mysql = require('mysql')
 function onloadBack(res) {
 
     var con = mysql.createConnection(sqlDetails)
-    con.query("SELECT * FROM `"+ process.env.BDNAME +"`.`productos`", function (err, result, fields) {
+    var arrayDB = con.query("SELECT * FROM `"+ process.env.BDNAME +"`.`productos`", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
+        return result
       });
 
     var arrayItems = ["nombre", "descripcion", "precio", "caracteristica1", "caracteristica2", "caracteristica3", "categoria"];
@@ -18,13 +19,13 @@ function onloadBack(res) {
 
     console.log(productItems)
 
-    productItems.push(arrayItems)
-    productItems.push(arrayItems)
+    productItems.push(arrayDB)
 
     console.log(productItems)
     res.sendFile(path.join(__dirname, "../public/index.html"))
 }
 
 module.exports = {
-    onloadBack
+    onloadBack,
+    arrayDB
 }
