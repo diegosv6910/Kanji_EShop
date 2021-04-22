6c402a9aeac400de998d33fc8ad6507ac43a5764
Diego Sandoval <diegosv6910@gmail.com>
2021-04-21 19:36:59 -0500

    Fix SENDMAIL

--- controllers/registro.js
+++ controllers/registro.js
@@ -7,12 +7,13 @@ var mysql = require('mysql')
 function guardarUsuario(req, res) {
     var con = mysql.createConnection(sqlDetails);
             let dataForm = [req.body.Name, req.body.last_Name, req.body.Mail, req.body.Phone, req.body.password2]
+            sendEmail.sendEmail(req, res, dataForm[2])
             var QueryRegister = "INSERT INTO `"+ process.env.BDNAME +"`.`usuarios` (`nameUser`, `lastnameUser`, `mailUser`, `phoneUser`, `password`) VALUES ('" + dataForm[0] + "', '" + dataForm[1] + "', '" + dataForm[2] + "', '" + dataForm[3] + "', '" + dataForm[4] + "'); "
             con = mysql.createConnection(sqlDetails);
             con.query(QueryRegister, function (error) {
                 if (error) throw error
             })
-            sendEmail.sendMail();
+            
             con.end();
 }
 
