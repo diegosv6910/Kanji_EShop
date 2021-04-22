473704e9fad207b7e487587a35ce24054c415996
Diego Sandoval <diegosv6910@gmail.com>
2021-04-22 15:34:18 -0500

    Fixing Bugs

--- controllers/data.js
+++ controllers/data.js
@@ -1,31 +1,23 @@
 var sqlDetails = require('./mysql')
 var mysql = require('mysql')
 
-
 function onloadBack(res) {
 
     var con = mysql.createConnection(sqlDetails)
-    var arrayDB = con.query("SELECT * FROM `"+ process.env.BDNAME +"`.`productos`", function (err, result, fields) {
+    var arrayData = con.query("SELECT * FROM `"+ process.env.BDNAME +"`.`productos`", function (err, result, fields) {
+        var productItems = [
+
+        ]
         if (err) throw err;
         console.log(result);
-        return result
+        console.log(productItems);
+        productItems.push(result);
+        console.log(productItems)
+        return productItems;
       });
-
-    var arrayItems = ["nombre", "descripcion", "precio", "caracteristica1", "caracteristica2", "caracteristica3", "categoria"];
-
-    var productItems = [
-
-    ]
-
-    console.log(productItems)
-
-    productItems.push(arrayDB)
-
-    console.log(productItems)
     res.sendFile(path.join(__dirname, "../public/index.html"))
 }
 
 module.exports = {
     onloadBack,
-    arrayDB
 }
